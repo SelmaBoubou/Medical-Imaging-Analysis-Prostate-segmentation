3e42fa153b56c0eb6f762f37ce42279c78bc1899
Selma.B <boudissa.selma@gmail.com>
2018-05-18 10:46:04 +0200

    update

--- code/prostate_segmentation.m
+++ code/prostate_segmentation.m
@@ -22,7 +22,7 @@ function varargout = prostate_segmentation(varargin)
 
 % Edit the above text to modify the response to help prostate_segmentation
 
-% Last Modified by GUIDE v2.5 15-May-2018 15:14:54
+% Last Modified by GUIDE v2.5 17-May-2018 00:35:38
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -124,5 +124,83 @@ function Save_Callback(hObject, eventdata, handles)
 % --- Executes on button press in Segmentation.
 function Segmentation_Callback(hObject, eventdata, handles)
 % hObject    handle to Segmentation (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+regionP = imfreehand();
+setColor(regionP,'g');
+
+
+% Create a binary image ("mask") from the ROI object.
+binaryImage = regionP.createMask();
+handles.binaryImagesZP(:,:,handles.sizeRegions)=binaryImage;
+
+% Get coordinates of the boundary of the freehand drawn region.
+structBoundaries = bwboundaries(binaryImage);
+xy=structBoundaries{1}; % Get n by 2 array of x,y coordinates.
+y = xy(:, 1); % Rows.
+x = xy(:, 2); % Columns.
+
+
+handles.boundaryImagesZP(:,:,handles.sizeRegions)=zeros(size(binaryImage));
+for i=1:size(x)
+    handles.boundaryImagesZP(y(i),x(i),handles.sizeRegions)=255;
+end
+
+% Mask the image outside the mask, and display it.
+% Will keep only the part of the image that's inside the mask, zero outside mask.
+[X, map] = dicomread([handles.pathname handles.filenames{1,handles.currentSegmentImage}]);
+grayImage = mat2gray(X);
+blackMaskedImage = grayImage;
+blackMaskedImage(~binaryImage) = 0;
+handles.blackMaskedImagesZP(:,:,handles.sizeRegions)=blackMaskedImage;
+
+% Now crop the image.
+handles.leftColumnZP(handles.sizeRegions) = min(x);
+handles.rightColumnZP(handles.sizeRegions) = max(x);
+handles.topLineZP(handles.sizeRegions) = min(y);
+handles.bottomLineZP(handles.sizeRegions) = max(y);
+handles.widthZP(handles.sizeRegions) = handles.rightColumnZP(handles.sizeRegions) - handles.leftColumnZP(handles.sizeRegions) + 1;
+handles.heightZP(handles.sizeRegions) = handles.bottomLineZP(handles.sizeRegions) - handles.topLineZP(handles.sizeRegions) + 1;
+
+guidata(hObject, handles);
+
+
+
+% --- Executes on button press in GraphCut.
+function GraphCut_Callback(hObject, eventdata, handles)
+% hObject    handle to GraphCut (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+global File;
+
+% energy function
+G = imageSegmenter(File)
+axes(handles.Output);
+imshow(G);
+
+% --- Executes on button press in Labelling.
+function Labelling_Callback(hObject, eventdata, handles)
+% hObject    handle to Labelling (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in LevelSet.
+function LevelSet_Callback(hObject, eventdata, handles)
+% hObject    handle to LevelSet (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% Display the DICE similarity coeff 
+%set(handles.text18, 'String', 'Loading Done!');
+%set(handles.text17,'String',info_original(1).InstanceNumber);
+
+
+% --- Executes on button press in DICE.
+function DICE_Callback(hObject, eventdata, handles)
+% hObject    handle to DICE (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
